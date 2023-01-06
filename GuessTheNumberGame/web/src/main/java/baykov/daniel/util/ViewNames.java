b46a4fe0420861fe709ca82cf3d052a423caa5f7
Daniel Baykov <danibaikov@abv.bg>
2023-01-06 20:05:31 +0200

    setting up Thymeleaf and Initial Template

---
+++ GuessTheNumberGame/web/src/main/java/baykov/daniel/util/ViewNames.java
@@ -0,0 +1,10 @@
+package baykov.daniel.util;
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames() {}
+}
