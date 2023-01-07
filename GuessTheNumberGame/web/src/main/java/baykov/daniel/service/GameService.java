7929bdcc60213c98401d7aa8212954f2a63b268d
Daniel Baykov <danibaikov@abv.bg>
2023-01-07 16:15:45 +0200

    adding Game Service

---
+++ GuessTheNumberGame/web/src/main/java/baykov/daniel/service/GameService.java
@@ -0,0 +1,14 @@
+package baykov.daniel.service;
+
+public interface GameService {
+
+    boolean isGameOver();
+
+    String getMainMessage();
+
+    String getResultMessage();
+
+    void checkGuess(int guess);
+
+    void reset();
+}
