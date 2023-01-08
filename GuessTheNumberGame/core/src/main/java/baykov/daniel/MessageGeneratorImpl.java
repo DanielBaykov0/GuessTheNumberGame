105c1bcc8ac18de9e1101712ad735af416f269b6
Daniel Baykov <danibaikov@abv.bg>
2023-01-08 18:06:41 +0200

    implementing Message Generator Internationalization Result Message

--- GuessTheNumberGame/core/src/main/java/baykov/daniel/MessageGeneratorImpl.java
+++ GuessTheNumberGame/core/src/main/java/baykov/daniel/MessageGeneratorImpl.java
@@ -13,6 +13,13 @@ public class MessageGeneratorImpl implements MessageGenerator{
 
     // == constants ==
     private static final String MAIN_MESSAGE = "game.main.message";
+    public static final String WIN = "game.win";
+    public static final String LOSE = "game.lose";
+    public static final String INVALID_RANGE = "game.invalid.range";
+    public static final String FIRST_GUESS = "game.first.guess";
+    public static final String HIGHER = "game.higher";
+    public static final String LOWER = "game.lower";
+    public static final String REMAINING = "game.remaining";
 
     // == fields ==
     private final Game game;
@@ -34,32 +41,27 @@ public class MessageGeneratorImpl implements MessageGenerator{
     // == public methods ==
     @Override
     public String getMainMessage() {
-//        return "Number is between " +
-//                game.getSmallest() +
-//                " and " +
-//                game.getBiggest() +
-//                ". Can you guess it?";
         return getMessage(MAIN_MESSAGE, game.getSmallest(), game.getBiggest());
     }
 
     @Override
     public String getResultMessage() {
         if (game.isGameWon()) {
-            return "You guessed it! The number was " + game.getNumber();
+            return getMessage(WIN, game.getNumber());
         } else if (game.isGameLost()) {
-            return "You lost. The number was " + game.getNumber();
+            return getMessage(LOSE, game.getNumber());
         } else if (!game.isValidNumberRange()) {
-            return "Invalid number range!";
+            return getMessage(INVALID_RANGE);
         } else if (game.getRemainingGuesses() == game.getGuessCount()) {
-            return "What is your first guess?";
+            return getMessage(FIRST_GUESS);
         } else {
-            String direction = "Lower";
+            String direction = getMessage(LOWER);
 
             if (game.getGuess() < game.getNumber()) {
-                direction = "Higher";
+                direction = getMessage(HIGHER);
             }
 
-            return direction + "! You have " + game.getRemainingGuesses() + " guesses left";
+            return getMessage(REMAINING, direction, game.getRemainingGuesses());
         }
     }
 
